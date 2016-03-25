27607261bbf6eaeafc46545022c98a4103a3b825
Ivan Blagojevic <Ivan Blagojevic>
2016-03-25 18:27:58 +0100

    Initial commit

---
+++ javascript/funkcije.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ivan on 25.3.2016..
+ */
+function ispisImena() {
+    document.write("Ivan Blagojevic ");
+}
+function cena(p1,p2) {
+   return p1*p2;
+   
+    
+}
+function ispisiTekst(tekst) {
+    document.write(tekst);
+}
+function idTest() {
+    document.getElementById("1").style.color = "red";
+    document.getElementById("1").style.fontSize = "larger";
+
+
+}
